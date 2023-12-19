37d00543c0f3246fe057cf1b9bd566d039532778
Marcelly Pereira <marcellyp120@gmail.com>
2023-12-19 00:47:33 -0300

    :sparkles: feat: Filtrar tarefas já concluídas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { View, Text, TouchableWithoutFeedback, Keyboard, FlatList, Alert } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  FlatList,
+  Alert,
+} from "react-native";
 import { Empty } from "../../components/Empty";
 import { Header } from "../../components/Header";
 import { Task, TaskProps } from "../../components/Task";
@@ -9,10 +17,14 @@ import { styles } from "./styles";
 export function Home() {
   const [tasks, setTasks] = useState<TaskProps[]>([]);
   const [newTask, setNewTask] = useState('');
+  const [showCompletedTasks, setShowCompletedTasks] = useState(false);
 
   function handleTaskAdd() {
-    if (newTask !== '' && newTask.length >= 5 ) {
-      setTasks(task => [{id: uuidv4(), title: newTask, isCompleted: false}, ...task]);
+    if (newTask !== '' && newTask.length >= 5) {
+      setTasks((prevTasks) => [
+        { id: uuidv4(), title: newTask, isCompleted: false },
+        ...prevTasks,
+      ]);
     } else {
       Alert.alert("Ops!", "A tarefa deve ter pelo menos 5 caracteres.");
     }
@@ -20,59 +32,81 @@ export function Home() {
   }
 
   function handleRemoveTask(id: string) {
-    Alert.alert("Remover Tarefa", "Tem certeza que você deseja remover essa tarefa?", [
-      {
-        text: 'Sim',
-        onPress: () => setTasks(prevState => (prevState.filter(task => task.id !== id))),
-        style: 'destructive'
-      },
-      {
-        text: 'Não',
-        style: 'cancel'
-      }
-    ])
+    Alert.alert(
+      "Remover Tarefa",
+      "Tem certeza que você deseja remover essa tarefa?",
+      [
+        {
+          text: 'Sim',
+          onPress: () =>
+            setTasks((prevTasks) =>
+              prevTasks.filter((task) => task.id !== id)
+            ),
+          style: 'destructive',
+        },
+        {
+          text: 'Não',
+          style: 'cancel',
+        },
+      ]
+    );
   }
 
   function handleTaskDone(id: string) {
-    setTasks(prevState => (prevState.map(task => task.id === id ? {
-      ...task,
-      isCompleted: !task.isCompleted
-    } : task)))
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, isCompleted: !task.isCompleted } : task
+      )
+    );
   }
 
   const tasksCreated = tasks.length;
-  const tasksDone = tasks.filter(task => task.isCompleted).length;
+  const tasksDone = tasks.filter((task) => task.isCompleted).length;
 
   return (
     <TouchableWithoutFeedback onPress={handleBlurWithKeyboard}>
-      <View style={styles.container} >
-        <Header task={newTask} onChangeText={setNewTask} onPress={handleTaskAdd} />
+      <View style={styles.container}>
+        <Header
+          task={newTask}
+          onChangeText={setNewTask}
+          onPress={handleTaskAdd}
+        />
         <View style={styles.tasksContainer}>
           <View style={styles.info}>
             <View style={styles.row}>
-              <Text style={styles.tasksCreated}>Criadas</Text>
+              <TouchableOpacity onPress={() => setShowCompletedTasks(false)}>
+                <Text style={styles.tasksCreated}>Criadas</Text>
+              </TouchableOpacity>
               <View style={styles.counterContainer}>
                 <Text style={styles.counterTasks}>{tasksCreated}</Text>
               </View>
             </View>
 
             <View style={styles.row}>
-              <Text style={styles.tasksDone}>Concluídas</Text>
+              <TouchableOpacity onPress={() => setShowCompletedTasks(true)}>
+                <Text style={styles.tasksDone}>Concluídas</Text>
+              </TouchableOpacity>
               <View style={styles.counterContainer}>
                 <Text style={styles.counterTasks}>{tasksDone}</Text>
               </View>
             </View>
           </View>
 
-          <FlatList 
-            data={tasks}
-            keyExtractor={tasks => tasks.id!}
-            renderItem={({item}) => (
-              <Task key={item.id} 
-              isCompleted={item.isCompleted} 
-              title={item.title} 
-              onRemove={() => handleRemoveTask(item.id!)} 
-                onTaskCheck={() => handleTaskDone(item.id!)} />
+          <FlatList
+            data={
+              showCompletedTasks
+                ? tasks.filter((task) => task.isCompleted)
+                : tasks
+            }
+            keyExtractor={(item) => item.id!}
+            renderItem={({ item }) => (
+              <Task
+                key={item.id}
+                isCompleted={item.isCompleted}
+                title={item.title}
+                onRemove={() => handleRemoveTask(item.id!)}
+                onTaskCheck={() => handleTaskDone(item.id!)}
+              />
             )}
             ListEmptyComponent={<Empty />}
           />
